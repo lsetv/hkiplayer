667269eaba06e502b0163fd0f1e7a4d70154734c
Ryan <ryan@lsetv.com>
2021-04-27 12:51:31 -0400

    document ready patch

--- lsetvplayer.js
+++ lsetvplayer.js
@@ -71,9 +71,19 @@ onLoadListener = function () {
 }
 player.onLoad(onLoadListener);
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  console.log('DOM is ready.')
-  hideError();
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+} 
+
+docReady(function() {
+  // DOM is loaded and ready for manipulation here
+  hideError()
 });
 
 function fullscreen() {
