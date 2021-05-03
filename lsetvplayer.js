263e45d04d9acc55226b4d1be35684f70f835cc4
Ryan <ryan@lsetv.com>
2021-05-03 14:10:36 -0400

    Branced and added videojs player

--- lsetvplayer.js
+++ lsetvplayer.js
@@ -1,93 +1,48 @@
 
-var player = WowzaPlayer.create("wowza_player_2021",
-    {
-    "license":"PLAY1-fjU9h-8md7H-6X8mf-ThvbB-dVPGw",
-    "sources":[{
-    "sourceURL":"https://cdn3.wowza.com/1/bHN2L0hwWjlpRGd0/VzcvaWhN/hls/live/playlist.m3u8?dvr"
-    },
-    {
-    "sourceURL":""
-    }],
-    "title":"Hellen Keller International Gala 2021",
-    "description":"",
-    "posterFrameURL": "https://www-hki-org-develop.go-vip.net/wp-content/uploads/2021/04/HKI-PreShow_Still-No_Counter.png",
-    "endPosterFrameURL": "https://www-hki-org-develop.go-vip.net/wp-content/uploads/2021/04/HKI-PreShow_Still-No_Counter.png",
-    "autoPlay":false,
-    "mute":false,
-    "volume":75,
-    "uiEnableDVR":"true",
-    "uiShowChannelBug": "true",
-    "uiChannelBugLocation": "topleft",
-
-    }
-);
-
-//     "liveStartTimestamp": "2021-05-04T21:00:00+00:00"
-
-function addDonate () {
-    var insertEl = document.getElementById('wowza_player_2021-ChannelBug');
-    if (insertEl != null) {
-    insertEl.classList.remove("wowza_player_2021-Hide");
-    insertEl.classList.add("wowza_player_2021-TopLeft", "wowza_player_2021-Show");
-    newElement = document.createElement('div');
-    newElement.innerHTML = "<button type='button' class='wp-block-button__link' id='donateBtn' data-open='modal1'>Donate </button>";
-    newElement.className = 'donateContainer';
-    insertEl.appendChild(newElement);
-
-    return newElement;
-  } 
-  // else {
-  //   // console.log('we are on mobile');
-  //   const playerContainer = document.getElementById('wowza_player_2021');
-  //   newElement = document.createElement('div');
-  //   newElement.innerHTML = "<button type='button' class='wp-block-button__link' id='donateBtn' data-open='modal1'>Donate </button>";
-  //   newElement.className = 'donateContainer';
-  //   playerContainer.parentNode.insertBefore(newElement, playerContainer.nextSibling);
-  // }
-}
-
-// function inline() {
-//     const video = document.getElementById('wowza_player_2021-Video');
-//     // console.log(video);
-//     const attribute = document.createAttribute('playsinline');
-//     video.setAttribute('playsinline', 'playsinline');
-//     video.setAttribute('webkit-playsinline', 'true');
-
-// }
-
-function hideError () {
-  console.log("hideerror")
-  const error = document.getElementById('give_error_error');
-  const patt = /^You are missing the donation id to view this donation receipt.$/
-  if (error != null){
-    if (error.innerText.match(patt)) {
-      error.style.display = "none";
-      console.log("called")
-    }
-  }
-}
-
-onLoadListener = function () {
-    addDonate();
-    modal();
+var Button = videojs.getComponent('Button');
+var MyButton = videojs.extend(Button, {
+  constructor: function() {
+    Button.apply(this, arguments);
+    this.addClass('special');
+    this.id ='donateBtn';
+    this.setAttribute('data-open','modal1');
+    this.el().innerHTML = "Donate";
     
-}
-player.onLoad(onLoadListener);
-
-function docReady(fn) {
-  // see if DOM is already available
-  if (document.readyState === "complete" || document.readyState === "interactive") {
-      // call on next available tick
-      setTimeout(fn, 1);
-  } else {
-      document.addEventListener("DOMContentLoaded", fn);
+  },
+  handleClick: function() {
+    modal();
   }
-} 
-
-docReady(function() {
-  // DOM is loaded and ready for manipulation here
-  hideError()
 });
+videojs.registerComponent('MyButton', MyButton);
+  
+  var options = {
+    control: true,
+    liveui: true,
+    fill: true,
+    sources: [{
+      src: 'https://cdn3.wowza.com/1/Mm9NMk9scTVjeHl4/VlNFL1NP/hls/live/playlist.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    bigPlayButton: true,
+    controlBar: {
+      volumePanel: {
+        inline: false,
+        vertical: true
+      }
+    }
+  };
+
+  var player = videojs('wowza_player_2021', options, function onPlayerReady() {
+    videojs.log('Your player is ready!');
+  
+    // In this context, `this` is the player that was created by Video.js.  
+  
+    // How about an event listener?
+    this.on('ended', function() {
+      videojs.log('Awww...over so soon?!');
+    });
+  });
+  player.addChild('myButton', {}); 
 
 function fullscreen() {
   var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
@@ -110,6 +65,7 @@ function fullscreen() {
 
 // # Modal stuff:
 function modal(){
+
   const openEls = document.querySelectorAll("[data-open]");
   const closeEls = document.querySelectorAll("[data-close]");
   const isVisible = "is-visible";
@@ -119,6 +75,7 @@ function modal(){
       const modalId = this.dataset.open;
       fullscreen();
       document.getElementById(modalId).classList.add(isVisible);
+      console.log( 'fired ');
     });
   }
 
@@ -141,3 +98,4 @@ function modal(){
     }
   });
 }
+modal();
